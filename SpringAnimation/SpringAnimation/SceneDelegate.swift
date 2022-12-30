08998276f4159538a0a1240d3df102106588056f
Orianka13 <odobrogor@gmail.com>
2022-12-30 19:06:00 +0400

    Создан проект

---
+++ SpringAnimation/SpringAnimation/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  SpringAnimation
+//
+//  Created by Олеся Егорова on 30.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
