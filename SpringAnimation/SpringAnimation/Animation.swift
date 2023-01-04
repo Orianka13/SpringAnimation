53110279246b97b301de60966805ca19af79663b
Orianka13 <odobrogor@gmail.com>
2023-01-04 15:38:37 +0400

    Добавлена модель

---
+++ SpringAnimation/SpringAnimation/Animation.swift
@@ -0,0 +1,17 @@
+//
+//  Animation.swift
+//  SpringAnimation
+//
+//  Created by Олеся Егорова on 04.01.2023.
+//
+
+import Foundation
+
+struct Animation {
+    let name: String
+    let preset: String
+    let curve: String
+    let force: String
+    let duration: String
+    let delay: String
+}
